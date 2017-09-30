f1f201291c3ad9fab8f5f9f60ba1cbddc07ec2f0
Lucas Sales <lucasalesvieira2@gmail.com>
2017-09-29 23:07:27 -0300

    mudanças feitas

---
+++ 2017_09_25/js/script.js
@@ -0,0 +1,178 @@
+function ativarBilheteria(){
+  $("div").addClass('sumir');
+  $("#divBilheteria").removeClass('sumir');
+  $("#divBilheteria").addClass('ativar');
+}
+function ativarOrcamento(){
+  $("div").addClass('sumir');
+  $("#divOrcamento").removeClass('sumir');
+  $("#divOrcamento").addClass('ativar');
+}
+function ativarLucro(){
+  $("div").addClass('sumir');
+  $("#divLucro").removeClass('sumir');
+  $("#divLucro").addClass('ativar');
+}
+function ativarGenero(){
+  $("div").addClass('sumir');
+  $("#divGenero").removeClass('sumir');
+  $("#divGenero").addClass('ativar');
+}
+
+function renderBilheteria(data, compare) {
+  d3.select("#bilheteria").selectAll("div.h-bar")
+    .data(data)
+    .enter()
+      .append("div")
+        .attr("class", "h-bar")
+        .append("span");
+
+  d3.select("#bilheteria").selectAll("div.h-bar")
+    .data(data)
+      .attr("class", "h-bar")
+        .style("width", function (d) {
+          return (d.Worldwide_Gross_M/4) + "px";
+        })
+      .select("span")
+        .text(function (d) {
+          return d.Film;
+        });
+  if(compare)
+    d3.select("#bilheteria")
+      .selectAll("div.h-bar")
+        .sort(compare);
+}
+
+var compareBilheteria = function (a, b) {
+  return a.Worldwide_Gross_M < b.Worldwide_Gross_M? 1:-1;
+};
+
+function renderOrcamento(data, compare){
+  d3.select("#orcamento").selectAll("div.h-bar")
+    .data(data)
+    .enter()
+      .append("div")
+        .attr("class", "h-bar")
+          .append("span");
+
+  d3.select("#orcamento").selectAll("div.h-bar")
+    .data(data)
+      .attr("class","h-bar")
+        .style("width", function (d){
+          return (d.Budget_M*4) + "px";
+        })
+      .select("span")
+        .text(function(d){
+          return d.Film;
+        });
+
+  d3.select("#orcamento").selectAll("div").style("background-color","orange");
+
+  if(compare)
+    d3.select("#orcamento")
+      .selectAll("div.h-bar")
+        .sort(compare);
+}
+
+var compareOrcamento = function (a, b) {
+    return a.Budget_M < b.Budget_M? 1:-1;
+};
+
+function renderLucro(data, compare){
+  d3.select("#lucro").selectAll("div.h-bar")
+    .data(data)
+    .enter()
+      .append("div")
+        .attr("class", "h-bar")
+          .append("span");
+
+  d3.select("#lucro").selectAll("div.h-bar")
+    .data(data)
+      .attr("class","h-bar")
+        .style("width", function (d){
+          return (d.Worldwide_Gross_M-d.Budget_M)/2 + "px";
+        })
+      .select("span")
+        .text(function(d){
+          return d.Film;
+        });
+
+  d3.select("#lucro").selectAll("div").style("background-color","green");
+  if(compare)
+    d3.select("#lucro")
+      .selectAll("div.h-bar")
+        .sort(compare);
+}
+
+var compareLucro = function (a, b) {
+    return (a.Worldwide_Gross_M-a.Budget_M) < (b.Worldwide_Gross_M-b.Budget_M)? 1:-1;
+};
+
+var generos = [];
+function fillGeneros(data){
+  var a = {
+    genero: "",
+    lucro: 0
+  }
+  a.genero = data[0].Genre;
+  a.lucro = data[0].Worldwide_Gross_M - data[0].Budget_M;
+  generos.push(a);
+  data.forEach(function(film){
+    contain(generos, film);
+  });
+  console.log(generos);
+}
+
+function contain(v, film){
+  var existe = false;
+  v.forEach(function(e){
+    if(e.genero == film.Genre){
+      if(e.genero == film.Genre){
+        existe = true;
+        e.lucro = e.lucro + film.Worldwide_Gross_M - film.Budget_M;
+      }
+    }
+  });
+  if(!(existe)){
+    var o = {
+      genero: "",
+      lucro: 0
+    }
+    o.genero = film.Genre;
+    o.lucro = film.Worldwide_Gross_M - film.Budget_M;
+    generos.push(o);
+  }
+}
+
+
+function renderGenero(data, compare){
+  fillGeneros(data);
+  d3.select("#genero").selectAll("div.h-bar")
+    .data(generos)
+    .enter()
+      .append("div")
+        .attr("class", "h-bar")
+          .append("span");
+
+  d3.select("#genero").selectAll("div.h-bar")
+    .data(generos)
+      .attr("class","h-bar")
+      .style("width", function (d){
+        return (d.lucro/10) + "px";
+      })
+      .select("span")
+        .text(function(d){
+          return d.genero;
+        });
+
+  d3.select("#genero").selectAll("div").style("background-color","pink");
+
+  if(compare)
+    d3.select("#genero")
+      .selectAll("div.h-bar")
+        .sort(compare);
+
+}
+var compareGeneroLucro = function (a, b) {
+  return (a.lucro) < (b.lucro)? 1:-1;
+};
